8c9588639c321e564b0d105add4c4d1e81967352
SheetalPunyani <130825058+SheetalPunyani@users.noreply.github.com>
2023-08-06 21:11:51 +0530

    Updated week2.c

--- week2.c
+++ week2.c
@@ -3,8 +3,7 @@
 //(This doesn't contain the use of pointers or other more advanced C topics)
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <time.h>
+
 
 char board[3][3];
 const char PLAYER1 = 'X';
